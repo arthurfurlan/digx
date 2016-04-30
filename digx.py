2f1140d31977333484ab561c268d0ceaf48ca7dc
Arthur Furlan <afurlan@configr.com>
2016-04-30 12:27:58 -0300

    PEP8 fixes.

--- digx.py
+++ digx.py
@@ -11,8 +11,7 @@
 # On Debian systems, you can find the full text of the license in
 # /usr/share/common-licenses/GPL-2
 
-
-from dns import resolver,reversename
+from dns import resolver, reversename
 import socket
 
 
@@ -20,11 +19,10 @@ class UsageError(Exception):
     pass
 
 
-## make it easier to resolve website hosts and its reverse addresses
+# make it easier to resolve website hosts and its reverse addresses
 class Digx(object):
 
     RETVAL_ERROR_USAGE = 1
-
 
     def __init__(self):
         self.retrieve_name = False
@@ -33,49 +31,45 @@ class Digx(object):
         self.retrieve_serv = False
         self.lookup_domain = ''
 
-
     def run(self, args):
         retval = self.parse_args(args)
         self.do_lookup()
 
-
     def display_usage(self):
         print 'USAGE: @TODO'
 
-
     def parse_args(self, args):
 
-        ## retrieve domain name entries
+        # retrieve domain name entries
         if 'ns' in args:
             self.retrieve_name = True
             args.remove('ns')
 
-        ## retrieve domain mail entries
+        # retrieve domain mail entries
         if 'mx' in args:
             self.retrieve_mail = True
             args.remove('mx')
 
-        ## retrieve domain text entries
+        # retrieve domain text entries
         if 'txt' in args:
             self.retrieve_text = True
             args.remove('txt')
 
-        ## empty and/or missing arguments
+        # empty and/or missing arguments
         if not args:
             raise UsageError('empty and/or missing arguments.')
 
         self.lookup_domain = args[0]
 
-
     def do_lookup(self):
 
-        ## lookup sequence of all domain names
+        # lookup sequence of all domain names
         query = resolver.query(self.lookup_domain)
         hosts = []
         for rdata in query.response.answer:
             hosts.append(str(rdata.name).rstrip('.'))
 
-        ## lookup final address and reverse dns
+        # lookup final address and reverse dns
         addr = []
         rdns = []
         query = resolver.query(hosts[-1], 'a')
@@ -85,33 +79,33 @@ class Digx(object):
             value = reversename.from_address(value)
             rdns.append(str(resolver.query(value, 'PTR')[0]).rstrip('.'))
 
-        ## do retrieve domain name entries
+        # do retrieve domain name entries
         if self.retrieve_name:
             query = resolver.query(hosts[-1], 'ns')
             for rdata in query.response.answer[0].items:
                 print 'name: %s' % str(rdata).rstrip('.')
             print '--'
 
-        ## do retrieve domain mail entries
+        # do retrieve domain mail entries
         if self.retrieve_mail:
             query = resolver.query(hosts[-1], 'mx')
             for rdata in query.response.answer[0].items:
                 print 'mail: %s' % str(rdata).rstrip('.').split(' ')[1]
             print '--'
 
-        ## do retrieve domain serv entries
+        # do retrieve domain serv entries
         if self.retrieve_text:
             query = resolver.query(hosts[-1], 'txt')
             for rdata in query.response.answer[0].items:
                 print 'text: %s' % str(rdata).rstrip('.')
             print '--'
 
-        ## print hosts, address and reverse dns
+        # print hosts, address and reverse dns
         hosts.extend(addr)
         for host in hosts:
             print 'host: %s' % host
         print '--'
-        for k,v in enumerate(rdns):
+        for k, v in enumerate(rdns):
             if len(rdns) > 1:
                 print 'rnds: %s (%s)' % (v, addr[k])
             else:
